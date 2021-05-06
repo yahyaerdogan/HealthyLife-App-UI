7e692ee2ec66080be8e31e389eff64dd279ebb4d
yahyaerdogan <yahyaerdogan1199@gmail.com>
2021-05-06 04:44:21 +0300

    basic dependencies installed

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {NavigationContainer, StackActions} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Home from './screens/Home';
+import Mission from './screens/Mission';
+
+export default class App extends Component {
+  render() {
+    return (
+      
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home}/>
+          <Stack.Screen name="Mission" component={Mission}/>
+
+        </Stack.Navigator>
+      </NavigationContainer>
+
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

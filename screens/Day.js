59543f8612519ca2d6aebaff8f2788b116e015b2
yahyaerdogan <yahyaerdogan1199@gmail.com>
2021-05-08 04:31:30 +0300

    day.js has been created and some styling

--- screens/Day.js
+++ screens/Day.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, StyleSheet, View } from 'react-native'
 
 export default class Day extends Component {
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
+      <View style={styles.box1} >
+        <View style={{ borderRadius: 10, padding: 5, backgroundColor: this.props.active }} >
+          <Text style={{ color: "#fff" }} >{this.props.dayname}</Text>
+        </View>
+
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  box1: {
+    flex: 1,
+    alignItems:"center",
+    justifyContent:"center"
+  }
+});
--- screens/Home.js
+++ screens/Home.js
@@ -1,26 +1,43 @@
 import React, { Component } from 'react'
 import { Text, View, StyleSheet, ImageBackground } from 'react-native'
-
+import Day from './Day'
 export default class Home extends Component {
+    state = {
+        color: "darkslategray",
+        activestate: "rgba(255, 255, 255, 0.291821)"
+    }
     render() {
         return (
             <View style={styles.container}>
                 <View style={styles.containerone}>
                     <View style={styles.boxone}>
-
                     </View>
                     <View style={styles.boxtwo}>
                         <Text style={styles.name} >Hi, Yahya</Text>
                         <Text style={styles.subtitle} >Here is your health</Text>
                     </View>
                     <View style={styles.boxthree}>
-                        <ImageBackground source={require("../assets/graphone.png")} style={{width:360, height:"100%"}}  />
+                        <ImageBackground source={require("../assets/graphone.png")} style={{ width: 360, height: "100%" }} />
                     </View>
                     <View style={styles.boxfour}>
+                        <Day dayname="Sun" />
+                        <Day dayname="Mon" />
+                        <Day dayname="Tue" />
+                        <Day dayname="Wed" />
+                        <Day dayname="Thu" active={this.state.activestate} />
+                        <Day dayname="Fri" />
+                        <Day dayname="Sat" />
 
                     </View>
                 </View>
                 <View style={styles.containertwo}>
+                    <View style={styles.line} ></View>
+                    <View style={styles.progress} >
+                        <Text style={styles.textone}>My progress</Text>
+                    </View>
+                    <View style={styles.cards}>
+
+                    </View>
 
                 </View>
             </View>
@@ -34,7 +51,7 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     containerone: {
-        flex: 1,
+        flex: 1.5,
     },
     containertwo: {
         flex: 1,
@@ -48,26 +65,47 @@ const styles = StyleSheet.create({
     },
     boxtwo: {
         flex: 0.8,
-        marginHorizontal:35,
+        marginHorizontal: 35,
     },
-    
-    boxthree:{
-        flex:2.5
+
+    boxthree: {
+        flex: 2.5
     },
-    boxfour:{
-        flex:0.5,
-        color:"#fff",
-        flexDirection:"row",
+    boxfour: {
+        flex: 0.5,
+        color: "#fff",
+        flexDirection: "row",
     },
-    name:{
-        fontSize:35,
-        color:"#fff",
-        fontWeight:"bold",
+    name: {
+        fontSize: 35,
+        color: "#fff",
+        fontWeight: "bold",
         letterSpacing: -0.5,
-        marginTop:5,
+        marginTop: 5,
     },
-    subtitle:{
-        fontSize:20,
-        color:"#fff",
+    subtitle: {
+        fontSize: 20,
+        color: "#fff",
+    },
+    line:{
+        width:66,
+        height:4,
+        backgroundColor:"#f4f0f0",
+        borderRadius:2,
+        marginVertical:25,
+        left:150
+    },
+    progress:{
+        left:50
+    },
+    textone:{
+        fontsize:20,
+        color:"#2d2d2d",
+        letterSpacing:-0.5
+    },
+    cards:{
+        flex:1,
+        marginTop:10,
+        marginHorizontal:30
     }
 })

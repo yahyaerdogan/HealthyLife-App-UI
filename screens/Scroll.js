4b9f1237872448e96aae980a8fe0fdec06b648a7
yahyaerdogan <yahyaerdogan1199@gmail.com>
2021-05-24 21:26:43 +0300

    style design complete

--- screens/Scroll.js
+++ screens/Scroll.js
@@ -5,26 +5,50 @@ import * as Animated from 'react-native-animatable';
 export default class Scroll extends Component {
   render() {
     return (
-      <Animated.View animation={this.props.animation} duration={1200} 
-      style={[styles.memo1, {backgroundColor:this.props.bgcolor}]} >
+      <Animated.View animation={this.props.animation} duration={1200}
+        style={[styles.memo1, { backgroundColor: this.props.bgcolor }]} >
 
-      <View style={styles.indsideone}>
+        <View style={styles.indsideone}>
 
-        <Text style={{fontSize:20, color:"#fff", width:100}}>this.props.title</Text>
-        
-        <Text style={{fontSize:14, color:"#fff", marginVertical:5}}>this.props.month</Text>
+          <Text style={{ fontSize: 20, color: "#fff", width: 100 }}>{this.props.title}</Text>
 
-      </View>
-<View style={styles.indsidetwo}>
+          <Text style={{ fontSize: 14, color: "#fff", marginVertical: 5 }}>{this.props.month}</Text>
 
-  <ImageBackground source={this.props.background} style={{width:100, height:100}} />
+        </View>
+        <View style={styles.indsidetwo}>
 
-</View>
+          <ImageBackground source={this.props.background} style={{ width: 100, height: 100 }} />
 
-<TouchableOpacity style={styles.indsidethree}>
-  <Image source={require("../assets/i_icon.png")} />
-</TouchableOpacity>
+        </View>
+
+        <TouchableOpacity style={styles.indsidethree}>
+          <Image source={require("../assets/i_icon.png")} />
+        </TouchableOpacity>
       </Animated.View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  memo1: {
+    flex: 1,
+    margin: 10,
+    borderRadius: 18
+  },
+  indsideone: {
+    flex: 1,
+    padding: 10
+  },
+  indsidetwo: {
+    flex: 2,
+    marginVertical: 10,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  indsidethree: {
+    flex: 1,
+    alignItems: "flex-end",
+    justifyContent: "center",
+    margin: 10
+  }
+})

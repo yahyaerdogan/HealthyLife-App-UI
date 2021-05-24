5fa16653aa7cf36dd0f9d37d5311c0af9dff8db0
yahyaerdogan <yahyaerdogan1199@gmail.com>
2021-05-24 21:27:39 +0300

    Scroll imported

--- screens/Mission.js
+++ screens/Mission.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet, ScrollView } from 'react-native';
 import * as Animatable from 'react-native-animatable';
+import Scroll from './Scroll';
 
 export default class Home extends Component {
     render() {
@@ -8,17 +9,41 @@ export default class Home extends Component {
             <View style={styles.container}>
                 <ScrollView
                     showsVerticalScrollIndicator={false}
-                    style={{flex:1}}
+                    style={{ flex: 1 }}
                     contentContainerStyle={{ width: "100%", height: "120%" }}>
                     <View style={styles.header}>
                         <Text style={styles.inline}>My Mission</Text>
                     </View>
                     <View style={styles.memos}>
-                       <ScrollView
-                        horizontal={true}
-                        contentContainerStyle={{width:"200%"}}
-                        showsVerticalScrollIndicator ={false}
+                        <ScrollView
+                            horizontal={true}
+                            contentContainerStyle={{ width: "200%" }}
+                            showsVerticalScrollIndicator={false}
                         >
+                            <Scroll bgcolor="#ffbe86"
+                                background={require("../assets/graphtwo.png")}
+                                title="Game Of Chess"
+                                month="Sep - Nov"
+                                animation="bounceInLeft"
+                            />
+                            <Scroll bgcolor="#7dc9e7"
+                                background={require("../assets/graphtwo.png")}
+                                title="100 km Jogging"
+                                month="Sep - Nov"
+                                animation="bounceInLeft"
+                            />
+                            <Scroll bgcolor="#ffbe86"
+                                background={require("../assets/graphtwo.png")}
+                                title="Netflix and Chill"
+                                month="Sep - Nov"
+                                
+                            />
+                            <Scroll bgcolor="#7dc9e7"
+                                background={require("../assets/graphtwo.png")}
+                                title="Video Games"
+                                month="Sep - Nov"
+                                
+                            />
                         </ScrollView>
                     </View>
                 </ScrollView>
@@ -29,26 +54,26 @@ export default class Home extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex:1,
-        backgroundColor:"#fff"
+        flex: 1,
+        backgroundColor: "#fff"
     },
-    header:{
-        flex:1,
-        left:25,
-        top:100
+    header: {
+        flex: 1,
+        left: 25,
+        top: 100
     },
-    inline:{
-        fontSize:38,
-        letterSpacing:-0.5,
-        fontWeight:"bold",
-        color:"#2E2E2E"
+    inline: {
+        fontSize: 38,
+        letterSpacing: -0.5,
+        fontWeight: "bold",
+        color: "#2E2E2E"
     },
-    memos:{
-        height:250,
-        position:"absolute",
-        width:"100%",
-        top:160,
-        marginLeft:10
+    memos: {
+        height: 250,
+        position: "absolute",
+        width: "100%",
+        top: 160,
+        marginLeft: 10
 
     }
 })
